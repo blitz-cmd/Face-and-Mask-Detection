8d22f55da97f3b111b973b37bf0cb1e434554447
blitz <srfrter133@gmail.com>
2020-11-28 00:19:40 +0530

    comment added

--- detection.py
+++ detection.py
@@ -5,6 +5,7 @@ from imutils.video import VideoStream
 from imutils.video import FPS
 import face_recognition
 import numpy as np
+import requests
 import imutils
 import pickle
 import time
@@ -75,6 +76,15 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	# locations
 	return (locs, preds)
 
+def send_message(name):
+    return requests.post(
+        "https://api.mailgun.net/v3/sandboxeca3ec8512864d7396d8e896a0217ec9.mailgun.org/messages",
+        auth=("api", "4c33a7caef7af745ab2a23d55da7dabe-2af183ba-9011c821"),
+        files = [("attachment", ("image.jpg", open("image.jpg", "rb").read()))],
+        data={"from": 'hello@example.com',
+            "to": ["srfrter133@gmail.com"],
+            "subject": "Face Identified!!",
+            "html": "<html>" + name + " is identified.  </html>"})
 
 
 prototxtPath = r"/home/blitz/face_and_mask_recognition/face_detector/deploy.prototxt"
@@ -83,33 +93,51 @@ faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 maskNet = load_model("mask_detector.model")
 print("mask detector...")
 
+#Initialize 'currentname' to trigger only when a new person is identified.
 currentname = "unknown"
+#Determine faces from encodings.pickle file model created from train_model.py
 encodingsP = "encodings.pickle"
+#use this xml file
 cascade = "haarcascade_frontalface_default.xml"
+# load the known faces and embeddings along with OpenCV's Haar
+# cascade for face detection
 print("[INFO] loading encodings + face detector...")
 data = pickle.loads(open(encodingsP, "rb").read())
 detector = cv2.CascadeClassifier(cascade)
 
+# initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
+# start the FPS counter
 fps = FPS().start()
 
+# loop over frames from the video file stream
 while True:
+	# grab the frame from the threaded video stream and resize it
+	# to 500px (to speedup processing)
 	frame = vs.read()
 	frame = imutils.resize(frame, width=500)
 	
 	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
 	
+	# convert the input frame from (1) BGR to grayscale (for face
+	# detection) and (2) from BGR to RGB (for face recognition)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	# detect faces in the grayscale frame
 	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
 		minNeighbors=5, minSize=(30, 30),
 		flags=cv2.CASCADE_SCALE_IMAGE)
+	# OpenCV returns bounding box coordinates in (x, y, w, h) order
+	# but we need them in (top, right, bottom, left) order, so we
+	# need to do a bit of reordering
 	boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+	# compute the facial embeddings for each face bounding box
 	encodings = face_recognition.face_encodings(rgb, boxes)
 	names = []
 
+	# loop over the facial & mask embeddings
 	for (encoding,pred,box) in zip(encodings,preds,locs):
 		# attempt to match each face in the input image to our known
 		# encodings
@@ -154,6 +182,13 @@ while True:
 			if currentname != name:
 				currentname = name
 				print(currentname)
+				#img_name = "image.jpg"
+				#cv2.imwrite(img_name, frame)
+				#print('Taking a picture.')
+				
+				#Now send me an email to let me know who is at the door
+				#requests = send_message(name)
+				#print ('Status Code: '+format(requests.status_code)) #200 status code means email sent successfully
 		
 		# update the list of names
 		names.append(name)
@@ -176,25 +211,13 @@ while True:
 		break
 
 	# update the FPS counter
-	#fps.update()
+	fps.update()
 
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
+# do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
